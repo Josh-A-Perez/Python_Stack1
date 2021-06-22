4a2b258cb356330fdd2135f83a7623cb5b15aa30
Josh-A-Perez <23joshperez30@gmail.com>
2021-06-22 13:24:07 -0700

    Bank Accounts

--- Python_Fundamentals/OOP/Bank_Account.py
+++ Python_Fundamentals/OOP/Bank_Account.py
@@ -1,41 +1,46 @@
 """
 Creating BankAccount class
+Add a deposit method to the BankAccount class
+Add a withdraw method to the BankAccount class
+Add a display_account_info method to the BankAccount class
 """
 
-class BankAccount(object):
-    def __init__(self,balance=0):
-        self.balance=balance
-    
-"""
-Method to show balance after depositing cash to bank account
-"""
+class BankAccount:
+    def __init__(self,interest_rate = 0, balance = 0):
+        self.int_rate = interest_rate
+        self.balance = balance
 
-def deposit(self,amount):
-    self.balance+= amount
-    return self.balance
+    def deposit(self,amount):
+            self.balance += amount
+            print(self.balance)
+            return self
 
-"""
-Method that updates balance after withdrawal,no transation if amount>balance
-"""
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("INVALID TRANSACTION")
+        elif self.balance > 0:
+            self.balance -= amount
+            print(self.balance)
+        return self
 
-def withdraw(self, amount):
-    if amount > self.balance:
-        return ("INVALID TRANSACTION")
-        self.balance-= amount
-        return self.balance
+    def display_account_info(self, amount):
+        print(self.balance)
+        return self
 
-"""
-Creating a subclass
-"""
+    def yield_interest(self, amount):
+        if self.balance > 0:
+            self.int_rate = amount * self.balance
+            print(self.int_rate + self.balance)
+        return self
 
-class MinimumBalanceAccount(BankAccount):
-    pass
+Josh = BankAccount(0,300)
 
+Josh.deposit(400)
+Josh.withdraw(30)
+Josh.yield_interest(.05)
 
-"""
--Test variables
--pondiAccount=BankAccount()
--faithAccount=MinimumBalanceAccount()
--pondiAccount.deposit(5000)
--faithAccount.deposit(40000)
-"""
+Rick = BankAccount(0,1000)
+
+Rick.deposit(200)
+Rick.withdraw(20)
+Rick.yield_interest(.02)
