67cf335d7532ae2b3601e66c52939e5186faf412
Josh-A-Perez <23joshperez30@gmail.com>
2021-06-22 16:55:56 -0700

    ajustments

--- Python_Fundamentals/OOP/Bank_Account.py
+++ Python_Fundamentals/OOP/Bank_Account.py
@@ -7,7 +7,7 @@ Add a display_account_info method to the BankAccount class
 
 class BankAccount:
     def __init__(self,interest_rate = 0, balance = 0):
-        self.int_rate = interest_rate
+        self.interest_rate = interest_rate
         self.balance = balance
 
     def deposit(self,amount):
@@ -23,24 +23,25 @@ class BankAccount:
             print(self.balance)
         return self
 
-    def display_account_info(self, amount):
+    def display_account_info(self):
         print(self.balance)
         return self
 
-    def yield_interest(self, amount):
+    def yield_interest(self):
         if self.balance > 0:
-            self.int_rate = amount * self.balance
-            print(self.int_rate + self.balance)
-        return self
+            interest_Amount = self.interest_rate * self.balance
+            self.balance += interest_Amount
+        return self   
 
-Josh = BankAccount(0,300)
+Josh = BankAccount(.03,1000)
+
 Josh.deposit(100)
-Josh.deposit(250)
-Josh.deposit(400)
-Josh.withdraw(30)
-Josh.yield_interest(.05)
+Josh.deposit(120)
+Josh.deposit(200)
+Josh.withdraw(60)
+Josh.yield_interest().display_account_info()
 
-Rick = BankAccount(0,1000)
+Rick = BankAccount(.02,1000)
 
 Rick.deposit(350)
 Rick.deposit(200)
@@ -49,4 +50,4 @@ Rick.withdraw(80)
 Rick.withdraw(100)
 Rick.withdraw(40)
 
-Rick.yield_interest(.02)
+Rick.yield_interest().display_account_info()
