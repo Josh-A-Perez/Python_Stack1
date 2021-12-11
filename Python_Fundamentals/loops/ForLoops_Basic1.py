2694910fc7a80167f50427426217d131591be58b
Josh-A-Perez <23joshperez30@gmail.com>
2021-12-11 14:35:21 -0800

    for loops 1

--- Python_Fundamentals/loops/ForLoops_Basic1.py
+++ Python_Fundamentals/loops/ForLoops_Basic1.py
@@ -1,20 +1,19 @@
-"""
-1. Basic - Print all integers from 0 to 150.
-"""
+#1. Basic - Print all integers from 0 to 150.
+
 for i in range(0, 151):
     print(i)
 
-"""
-2. Multiples of Five - Print all the multiples of 5 from 5 to 1,000
-"""
+
+#2. Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+
 for i in range(0, 1001):
     if i%5 == 0:
         print(i)
 
-"""
-3. Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" 
+
+#3. Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" 
 instead. If divisible by 10, print "Coding Dojo".
-"""
+
 for i in range(0, 101):
     if i%5 == 0 and i%10 == 0:
         print("Coding Dojo")
@@ -23,18 +22,16 @@ for i in range(0, 101):
     else:
         print(i)
 
-"""
-4. Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
-"""
+#4. Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+
 sum = 0
 for x in range(0, 5000000):
     if x%2 != 0:
         sum += x
 print(sum)
 
-"""
-5. Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
-"""
+#5. Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
 for x in range(2018, 0, -4):
     if x > 0:
         print(x)
